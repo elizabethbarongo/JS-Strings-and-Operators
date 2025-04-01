e0888beba212568358594e022b44ddf0d8c92896
Elizabeth Barongo <elizabethmoraab@gmail.com>
2025-04-01 10:53:28 +0300

    added a file

--- README.md
+++
@@ -1,1 +0,0 @@
-# JS-Strings-and-Operators
---
+++ strings.js
@@ -0,0 +1,43 @@
+let statement = `The child walked to the market`;
+let newstatement = statement.charAt(0).toUpperCase()+statement.slice(1);
+console.log({newstatement})
+
+let word = 'The child walked to the market';
+let newword = 'The' + 'f' + 'child walked to the market';
+console.log({newword});
+
+
+let b = "hello world, hello JavaScript!";
+let newb = "hello";
+let total = b.split (newb). length -1;
+console.log({total});
+
+let c = "happy" ;
+let newc = c.slice(2,);
+console.log({newc});
+
+let e = "message";
+let newe = e.slice(0,3);
+console.log({newe});
+
+let g = 'malaria';
+let newg = g.split(''). sort() . join('');
+console.log ({newg});
+
+let m = "I love eating pilau";
+let newm = m.split(" "). join ("");
+console.log({newm});
+
+let y = 'greeting';
+console.log(y.indexOf('t'));
+
+let x = 'destination';
+let newx=x.substring(2,11,x.length-1);
+console.log({newx});
+
+
+let v = 'journey';
+let newv=v.substring(0,5 ,v.length-1);
+console.log({newv});
+
+
